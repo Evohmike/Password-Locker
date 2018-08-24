4a75c0b00a4172d051b0c36bc3ecfd3c8d20f9e1
Evohmike <evohmike@gmail.com>
2018-08-24 23:08:38 +0300

    creaded a tearDown function that cleans up each test case has run.

--- creds_test.py
+++ creds_test.py
@@ -9,6 +9,14 @@ class TestCredentials(unittest.TestCase):
         Set up method to run before each test cases.
         '''
         self.new_credentials = Credentials("Evoh","Evohmike","evohmike@py.com","12345")
+
+
+    def tearDown(self):
+        '''
+        This method cleans up after each test case has run
+        '''
+        Credentials.credentials_list = []
+
     def test_credentials_init(self):
         '''
         This tests if the object has been initialized properly
@@ -18,6 +26,22 @@ class TestCredentials(unittest.TestCase):
         self.assertEqual(self.new_credentials.email, "evohmike@py.com")
         self.assertEqual(self.new_credentials.password, "12345")
 
-        
+    def test_save_credentials(self):
+        '''
+        Test if the credentials objects are saved into the credentials list
+        '''
+        self.new_credentials.save_credentials()
+        self.assertEqual(len(Credentials.credentials_list),1)
+
+    def test_save_multiple_credentials(self):
+        '''
+        This checks if we can save multiple credentials objects into the credentials list.
+        '''
+        self.new_credentials.save_credentials()
+        test_credentials = Credentials("Peter","petero","peter@py.com","abcdef")
+        test_credentials.save_credentials()
+        self.assertEqual(len(Credentials.credentials_list),2)
+
+
 if __name__=='__main__':
     unittest.main()
