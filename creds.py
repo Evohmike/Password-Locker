6e48f345459de81a389899708751a803fa788319
Evohmike <evohmike@gmail.com>
2018-08-24 23:16:25 +0300

    Created a method that defines the properties of the user credentilas.

--- creds.py
+++ creds.py
@@ -6,17 +6,18 @@ class Credentials:
     '''
     credentials_list = []
 
-  def __init__ (self,account_name,user_name,email,password):
-      '''
-      This method defines the properties of the user credentials.
-      '''
-      self.account_name = account_name
-      self.user_name = user_name
-      self.email = email
-      self.password = password
-    def save_credentials(self):
-        '''
-        This saves credential objects into credential list
-        '''
 
-        Credentials.credentials_list.append(self)
+def __init__(self, account_name, user_name, email, password):
+    '''
+    This method defines the properties of the user credentials.
+    '''
+    self.account_name = account_name
+    self.user_name = user_name
+    self.email = email
+    self.password = password
+  def save_credentials(self):
+      '''
+      This saves credential objects into credential list
+      '''
+
+      Credentials.credentials_list.append(self)
